d86a41fa556bcfba8098e7f2dda72571e20bbf92
BlakJack <BlakJack@users.noreply.github.com>
2014-03-18 23:23:39 +0530

    Update stuff.js

--- stuff/stuff.js
+++ stuff/stuff.js
@@ -30,9 +30,6 @@ exports.stuff = function (s) {
             }
             if (room.isPrivate) return ' ' + this.name;
         }
-        if (this.away) {
-            return this.group + this.name + '(Away)';
-        }
         if (this.hiding) {
             return this.hideSym + this.name;
         }
@@ -623,69 +620,12 @@ regdate: function(target, room, user, connection) {
 		req.end();
 	},
 	
-	afk: 'away',
-	away: function(target, room, user, connection) {
-		if (!this.can('lock')) return false;
-
-		if (!user.isAway) {
-			var originalName = user.name;
-			var awayName = user.name + ' - Away';
-			//delete the user object with the new name in case it exists - if it does it can cause issues with forceRename
-			delete Users.get(awayName);
-			user.forceRename(awayName, undefined, true);
-
-			this.add('|raw|-- <b><font color="#4F86F7">' + originalName +'</font color></b> is now away. '+ (target ? " (" + target + ")" : ""));
-
-			user.isAway = true;
-		}
-		else {
-			return this.sendReply('You are already set as away, type /back if you are now back');
-		}
-
-		user.updateIdentity();
-	},
-
-	back: function(target, room, user, connection) {
-		if (!this.can('lock')) return false;
-
-		if (user.isAway) {
-
-			var name = user.name;
-
-			var newName = name.substr(0, name.length - 7);
-
-			//delete the user object with the new name in case it exists - if it does it can cause issues with forceRename
-			delete Users.get(newName);
-
-			user.forceRename(newName, undefined, true);
-
-			//user will be authenticated
-			user.authenticated = true;
-
-			this.add('|raw|-- <b><font color="#4F86F7">' + newName + '</font color></b> is no longer away');
-
-			user.isAway = false;
-		}
-		else {
-			return this.sendReply('You are not set as away');
-		}
-
-		user.updateIdentity();
-	},
+	
     crai: 'cry',
     cry: function (target, room, user) {
         return this.parse('/me starts tearbending dramatically like Katara.');
     },
-    back: function (target, room, user) {
-        if (!user.away) {
-            this.sendReply('You are not even away.');
-            return false;
-        } else {
-            user.away = false;
-            this.add(user.name + ' is now back.');
-            user.updateIdentity();
-        }
-    },
+    
     dk: 'dropkick',
     dropkick: function (target, room, user) {
         if (!target) return this.sendReply('/dropkick needs a target.');
